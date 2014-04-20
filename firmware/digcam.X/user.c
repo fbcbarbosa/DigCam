91940d063de53f9e7eecc65eaa73885bce655d3e
fbcbarbosa <fbcbarbosa@gmail.com>
2014-04-20 01:15:58 -0300

    Changed project name + some basic MPLAB setup

---
+++ firmware/digcam.X/user.c
@@ -0,0 +1,26 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+#include <p24Fxxxx.h>      /* Device header file */
+
+#include <stdint.h>          /* For uint32_t definition */
+#include <stdbool.h>         /* For true/false definition */
+
+#include "user.h"            /* variables/params used by user.c */
+
+/******************************************************************************/
+/* User Functions                                                             */
+/******************************************************************************/
+
+/* <Initialize variables in user.h and insert code for user algorithms.> */
+
+/* TODO Initialize User Ports/Peripherals/Project here */
+
+void InitApp(void)
+{
+    /* Setup analog functionality and port direction */
+
+    /* Initialize peripherals */
+}
+
